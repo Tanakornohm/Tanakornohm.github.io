00d278d73d4235a7c031d0d331abc98d47c85e2d
boomNDS <boomsixboom@gmail.com>
2018-10-31 14:10:14 +0700

    add bmi section

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
-// When the user scrolls down 20px from the top of the document, slide down the navbar
-// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
 $(window).scroll(function() {scrollFunction()});
 function scrollFunction() {
   var windscroll = $(window).scrollTop();
-  if (windscroll >= 30) {
+  if (windscroll >= 100) {
     $("nav").css("top", "0");
   } else {
     $("nav").css("top", "-50px");
